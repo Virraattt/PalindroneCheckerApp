dcdd53657d919533d856298dc60e372412dd798d
Saimon Shaji <ss0175@srmist.edu.in>
2026-03-06 15:13:07 +0530

    Virat Added UC8 LinkedList Palindrome

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,24 +1,27 @@
+import java.util.LinkedList;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "madam";
+        String input = "level";
 
-        boolean result = check(input, 0, input.length() - 1);
+        LinkedList<Character> list = new LinkedList<>();
 
-        System.out.println("Is Palindrome: " + result);
-    }
-
-    private static boolean check(String s, int start, int end) {
-
-        if (start >= end) {
-            return true;
+        for (char c : input.toCharArray()) {
+            list.add(c);
         }
 
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
+        boolean isPalindrome = true;
+
+        while (list.size() > 1) {
+            if (!list.removeFirst().equals(list.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        return check(s, start + 1, end - 1);
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome: " + isPalindrome);
     }
 }
